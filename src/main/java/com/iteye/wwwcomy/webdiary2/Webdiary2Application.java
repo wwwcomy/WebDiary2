a479d0a4fe23650a430e2667280e880671c73616
xingnliu <xingnliu@dhcp-10-140-42-158.cisco.com>
2021-03-09 15:23:54 +0800

    initial submit

---
+++ src/main/java/com/iteye/wwwcomy/webdiary2/Webdiary2Application.java
@@ -0,0 +1,13 @@
+package com.iteye.wwwcomy.webdiary2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webdiary2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webdiary2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iteye/wwwcomy/webdiary2/Webdiary2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iteye.wwwcomy.webdiary2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webdiary2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
