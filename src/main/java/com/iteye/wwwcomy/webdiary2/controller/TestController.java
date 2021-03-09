f356a4c8e1885cce79074c9634c5203c47890f18
xingnliu <xingnliu@dhcp-10-140-42-158.cisco.com>
2021-03-09 19:21:26 +0800

    Add DB connection config

---
+++ src/main/java/com/iteye/wwwcomy/webdiary2/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.iteye.wwwcomy.webdiary2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String test1() {
+		return "This is a test";
+	}
+}
