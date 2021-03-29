7a3fd5b091cce58591e8aa9e4d2371bbc33f95dd
xingnliu <xingnliu@192.168.6.217>
2021-03-29 15:43:55 +0800

    Add one log

--- src/main/java/com/iteye/wwwcomy/webdiary2/Webdiary2Application.java
+++ src/main/java/com/iteye/wwwcomy/webdiary2/Webdiary2Application.java
@@ -1,13 +1,18 @@
 package com.iteye.wwwcomy.webdiary2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Webdiary2Application {
 
+	private static Logger logger = LoggerFactory.getLogger(Webdiary2Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Webdiary2Application.class, args);
+		logger.info("Started, swagger-ui Endpoint: localhost:port/swagger-ui/");
 	}
 
 }
